623b85050b3fae66deaa1f360bfe531dfdac33c4
danielperr <daniel.perez079@gmail.com>
2019-02-22 15:58:26 +0200

    Modified server.py

--- server.py
+++ server.py
@@ -1,33 +1,38 @@
-import socket as s
-import threading, thread
+import socket as s 
+import threading
+import thread
 
 BUFFER_SIZE = 1024
 
 HOST = 'localhost'
-PORT = 50008
+PORT = 30000
 ADDRESS = (HOST, PORT)
 
+
+# ---------- NETWORK ----------
+
 def handle_connection(client_socket, address):
-    print '[*] Started communication with :', address
+    print '[v] Started communication with :', address
 
     while True:
         data = client_socket.recv(BUFFER_SIZE)
         if not data: break
-        msg = data
+        msg = 'echoed:... ' + data
         client_socket.send(msg)
     
+    print '[x] Ended communication with :' , address
     client_socket.close()
-    print '[*] Ended communication with :', address
 
 
 def main():
-
     server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
     server_socket.bind(ADDRESS)
     server_socket.listen(2)
-
     print '[*] Listening on port %s . . .' % PORT
 
     while True:
         client_socket, address = server_socket.accept()
         thread.start_new_thread(handle_connection, (client_socket, address))
+
+if __name__ == '__main__':
+    print help()
